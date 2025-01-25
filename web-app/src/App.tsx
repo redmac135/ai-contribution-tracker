d3d28ef93d47c476e584f63b8a86d2832ddf3d0a
itsxilef <flxj1363@gmail.com>
2025-01-24 21:40:58 -0500

    frontend changes

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SpinningAnimation } from './components/SpinningAnimation';
 
 function App() {
+
+  const [recording, setRecording] = useState(false);
+  const [media, setMedia] = useState<Blob[]>([]);
+  const[mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
+
+  async function uploadAudio(blob: Blob) {
+		const formData = new FormData();
+		formData.append('audio', blob);
+		// Tmp user id
+		formData.append('useruid', '1');
+		return await fetch(`${process.env.REACT_APP_BACKEND_URL}entries/create/`, {
+			method: 'POST',
+			body: formData
+		})
+	}
+
+	useEffect(() => {
+		const setupMediaDevice = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      const tmpMediaRecorder = new MediaRecorder(stream);
+		  tmpMediaRecorder.ondataavailable = function (e) {
+        setMedia(media => [...media, e.data]);
+		  };
+		  tmpMediaRecorder.onstop = function () {
+		  	const blob = new Blob(media, { type: 'audio/ogg' });
+		  	uploadAudio(blob);
+		  };
+		  setMediaRecorder(tmpMediaRecorder);
+    }
+
+    setupMediaDevice();
+	}, []);
+
+  function startRecording() {
+    console.log('started recording');
+    (mediaRecorder as MediaRecorder).start();
+  }
+  function stopRecording() {
+		console.log('stopped recording');
+		(mediaRecorder as MediaRecorder).stop();
+	}
+  function handleRecordingToggle() {
+		// note: this value is reversed as handle change gets called before bind is called.
+    if (recording) {
+      stopRecording();
+      setRecording(false);
+		} else {
+      startRecording();
+      setRecording(true);
+		}
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <label className="recordLabel" htmlFor="recordButton">fdjsghakaghsdkjf</label>
+        <input type='checkbox' id='recordButton' checked={recording} onChange={handleRecordingToggle} />
+
+        <SpinningAnimation/>
       </header>
     </div>
+
   );
 }
 
---
+++ web-app/src/components/SpinningAnimation.tsx
@@ -0,0 +1,14 @@
+import './SpinningAnimation.css';
+
+export function SpinningAnimation(){
+
+    return(
+        <div id="container">
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="h3">loading</div>
+        </div>
+    )
+}
