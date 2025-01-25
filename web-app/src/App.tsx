45fd16b54a74da96d59272c990179f4944156522
itsxilef <flxj1363@gmail.com>
2025-01-24 23:41:53 -0500

    having mic button & better ui

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,70 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './checkButton.css';
+import './micButton.css';
 import { SpinningAnimation } from './components/SpinningAnimation';
 
 function App() {
 
   const [recording, setRecording] = useState(false);
   const [media, setMedia] = useState<Blob[]>([]);
-  const[mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
 
   async function uploadAudio(blob: Blob) {
-		const formData = new FormData();
-		formData.append('audio', blob);
-		// Tmp user id
-		formData.append('useruid', '1');
-		return await fetch(`${process.env.REACT_APP_BACKEND_URL}entries/create/`, {
-			method: 'POST',
-			body: formData
-		})
-	}
+    const formData = new FormData();
+    formData.append('audio', blob);
+    // Tmp user id
+    formData.append('useruid', '1');
+    return await fetch(`${process.env.REACT_APP_BACKEND_URL}entries/create/`, {
+      method: 'POST',
+      body: formData
+    })
+  }
 
-	useEffect(() => {
-		const setupMediaDevice = async () => {
+  useEffect(() => {
+    const setupMediaDevice = async () => {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       const tmpMediaRecorder = new MediaRecorder(stream);
-		  tmpMediaRecorder.ondataavailable = function (e) {
+      tmpMediaRecorder.ondataavailable = function (e) {
         setMedia(media => [...media, e.data]);
-		  };
-		  tmpMediaRecorder.onstop = function () {
-		  	const blob = new Blob(media, { type: 'audio/ogg' });
-		  	uploadAudio(blob);
-		  };
-		  setMediaRecorder(tmpMediaRecorder);
+      };
+      tmpMediaRecorder.onstop = function () {
+        const blob = new Blob(media, { type: 'audio/ogg' });
+        uploadAudio(blob);
+      };
+      setMediaRecorder(tmpMediaRecorder);
     }
 
     setupMediaDevice();
-	}, []);
+  }, []);
 
   function startRecording() {
     console.log('started recording');
     (mediaRecorder as MediaRecorder).start();
   }
   function stopRecording() {
-		console.log('stopped recording');
-		(mediaRecorder as MediaRecorder).stop();
-	}
+    console.log('stopped recording');
+    (mediaRecorder as MediaRecorder).stop();
+  }
   function handleRecordingToggle() {
-		// note: this value is reversed as handle change gets called before bind is called.
+    // note: this value is reversed as handle change gets called before bind is called.
     if (recording) {
       stopRecording();
       setRecording(false);
-		} else {
+    } else {
       startRecording();
       setRecording(true);
-		}
+    }
   }
 
 
   return (
     <div className="App">
       <header className="App-header">
-
-        <label className="recordLabel" htmlFor="recordButton">fdjsghakaghsdkjf</label>
-        <input type='checkbox' id='recordButton' checked={recording} onChange={handleRecordingToggle} />
-
-        <SpinningAnimation/>
+        <div className="title">Start Tracking Participation</div>
+        {recording && <label className="recordLabel" htmlFor="recordButton" ><SpinningAnimation /></label>}
+        {!recording && <label className="micNotOn" htmlFor="recordButton">
+          <img className="micButton" src="/voice.png" alt="" />
+        </label>}
+        <input className="checkButton" type='checkbox' id='recordButton' checked={recording} onChange={handleRecordingToggle} />
+        <div className="rectangle"></div>
       </header>
     </div>
 
