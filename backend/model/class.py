0268be8b68794541e7d6decb5b05115a6b9b1fd8
itsxilef <flxj1363@gmail.com>
2025-04-02 16:34:45 -0400

    schema on backend

---
+++ backend/model/class.py
@@ -0,0 +1,13 @@
+from mongoengine import Document, StringField, ListField, ReferenceField, ObjectIdField
+
+class Class(Document):
+    name = StringField(required=True)
+    students = ListField(StringField(), required=True)  # List of student names
+    lectures = ListField(ObjectIdField(), required=True)  # List of lecture IDs (MongoDB ObjectIds)
+
+    meta = {
+        'collection': 'classes',  # MongoDB collection name
+        'ordering': ['name'],     # Default ordering by name
+        'auto_create_index': True
+    }
+
