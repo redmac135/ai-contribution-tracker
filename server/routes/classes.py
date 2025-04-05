29d464496a19c10c6cf585fcd84579e68c9e4518
itsxilef <flxj1363@gmail.com>
2025-04-05 11:48:31 -0400

    classes change

--- server/routes/classes.py
+++ server/routes/classes.py
@@ -1,3 +1,13 @@
 from fastapi import APIRouter
+from models.Class import Class
 
 router = APIRouter()
+
+@router.get("/info/")
+def classes(className: str):
+    classthing = Class.objects(name=className).first()
+    print(classthing)
+
+    return {
+        "message": "Hello from the classes route!"
+    }
