18bd30b9045cc03dd8c5be079b6ced13f72f29aa
Ethan Zhao <ethanzhao1981@gmail.com>
2025-04-05 14:46:57 -0400

    add request to backend endpoint

--- server/routes/recognition.py
+++ server/routes/recognition.py
@@ -21,8 +21,19 @@ UPLOAD_DIR = "audio_uploads"
 Path(UPLOAD_DIR).mkdir(exist_ok=True)
 
 
+@router.get("/classnames/")
+async def classNames():
+    """
+    Returns a list of all class names in the database.
+    """
+    class_names = Class.objects().distinct("name")
+    return {"class_names": class_names}
+
+
 @router.post("/convert/")
-async def upload_and_transcribe(request: Request, file: UploadFile = File(...)):
+async def upload_and_transcribe(
+    request: Request, section: str, file: UploadFile = File(...)
+):
     model = request.app.state.whisper_model
 
     raw_mime: str | None = file.content_type
@@ -78,7 +89,48 @@ async def upload_and_transcribe(request: Request, file: UploadFile = File(...)):
     said = transcription[len(name) + 1 :]
 
     # upload to DB
-    
+
     # 1. Check if the name already exists in the database
+    class_object = Class.objects(name=section).first()
+
+    if not class_object:
+        class_object = Class(name=section, students=[name]).save()
+
+    students = class_object.students
     # 2. If it does not, add the student to the class list
-    Class.objects(name=)
+    if name not in students:
+        class_object.students.append(name)
+        class_object.save()
+
+    # 3. Check if lecture object exists for this day
+    lecture_object = Lecture.objects(
+        class_name=section, date=time.strftime("%Y-%m-%d")
+    ).first()
+
+    if not lecture_object:
+        lecture_object = Lecture(
+            class_name=section,
+            date=time.strftime("%Y-%m-%d"),
+            contrib=[
+                {
+                    "name": name,
+                    "said": said,
+                    "score": 1,
+                }
+            ],
+        )
+
+        lecture_object.save()
+
+    else:
+        # 4. If it does, add the contribution to the lecture object
+        lecture_object.contrib.append(
+            {
+                "name": name,
+                "said": said,
+                "score": 1,
+            }
+        )
+        lecture_object.save()
+
+    return {"name": name, "said": said}
